70442ec0f71c0b158147ca47f61d2d067c138046
Muran94 <headiv94@gmail.com>
2017-11-19 01:03:04 +0900

    クライアントバリデーションの実装

--- vue.js
+++ vue.js
@@ -10,11 +10,18 @@ var vm = new Vue({
   el: "#bpm_calc_app",
   data: {
     original_bpm: null,
+    invalid: null,
     bpm_list: []
   },
   methods: {
     calc: function() {
       this.bpm_list = []; // 一度リストをリセットする
+      if (this.original_bpm.match(/^[0-9]{1,3}$/)) {
+        this.invalid = false;
+      } else {
+        this.invalid = true;
+        return;
+      }
       for (var mul_num = 0.6; mul_num <= 2.0; mul_num += 0.2) {
         this.bpm_list.push({
           speed: mul_num.toFixed(1) + "倍速",
